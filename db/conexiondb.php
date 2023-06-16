f16e8891699410834e43a576e40c77df91ba6582
elvirams <114144314+elvirams@users.noreply.github.com>
2023-06-16 17:28:29 +0200

    Update conexiondb.php

--- db/conexiondb.php
+++ db/conexiondb.php
@@ -1,31 +1,16 @@
 <?php
-    # Método para realizar la conexión a la BBDD. Obtiene los datos de conexión de variables de entorno existentes en el fichero .env.
-    class Database {
-        public static function connect () 
-        {
-            #$host='mariadb';
-            #$dbname=getenv('MARIADB_DATABASE');
-            $servername = "servidormariadbtfg.mariadb.database.azure.com";
-            $username = "adminazure@servidormariadbtfg";
-            $password = "emorcillos06@iesalbarregas.es";
-            $database = "moviles";
+class Database {
+    public static function connect() {
+        $host = 'servidormariadbtfg.mariadb.database.azure.com';
+        $dbname = 'moviles';
 
-            try {
-                #$dsn = 'mysql:host='.$host.";dbname=".$dbname.";charset=UTF8";
-                #$dbh = new PDO($dsn, getenv('MARIADB_USER'), getenv('MARIADB_PASSWORD'));
-                #return $dbh;
-                // Crear una conexión
-                $conn = new mysqli($servername, $username, $password, $database);
-                return $conn
-                    
-                // Verificar la conexión
-                if ($conn->connect_error) {
-                    die("Error de conexión: " . $conn->connect_error);
-                }
-                
-            } catch (PDOException $e){
-                echo $e->getMessage();
-            }
+        try {
+            $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=UTF8';
+            $dbh = new PDO($dsn, 'adminazure@servidormariadbtfg', 'emorcillos06@iesalbarregas.es');
+            return $dbh;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
+}
 ?>
